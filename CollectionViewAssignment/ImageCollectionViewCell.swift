f80f277c283626384dad3aa5eea42ef61d604879
Minami Munakata <june28munazo@gmail.com>
2018-10-29 09:19:28 -0700

    removed -> self.collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)

---
+++ CollectionViewAssignment/ImageCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ImageCollectionViewCell.swift
+//  CollectionViewAssignment
+//
+//  Created by minami on 2018-10-29.
+//  Copyright © 2018 宗像三奈美. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+}
